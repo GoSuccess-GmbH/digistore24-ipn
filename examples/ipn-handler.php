e834afbd7f42032d19878e38745409ba07f5be33
Paul Gossen <paul@gosuccess.io>
2026-06-21 16:44:28 +0200

    fix: correct vendor/autoload path in ipn-handler example
    The example lives in examples/, so it must require ../vendor/autoload.php.

--- examples/ipn-handler.php
+++ examples/ipn-handler.php
@@ -9,7 +9,7 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use GoSuccess\Digistore24\Ipn\{Notification, Response};
 use GoSuccess\Digistore24\Ipn\Enum\Event;
